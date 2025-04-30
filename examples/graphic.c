8f39acd636988e345ddfa19e1abc56871ca0e923
Amadey Vorontsov <vorontsov.aa@phystech.edu>
2025-04-30 23:36:42 +0300

    Update README.md

--- examples/graphic.c
+++ examples/graphic.c
@@ -2,7 +2,7 @@ void app() {
   for (int step = 0; step < 1000; ++step) {
     for (int y = 0; y < 1080; ++y)
       for (int x = 0; x < 1920; ++x)
-        simPutPixel(x, y, 0xFF000000 + x * y * step);
-    simFlush();
+        mozartVMPutPixel(x, y, 0xFF000000 + x * y * step);
+    mozartVMFlush();
   }
 }
